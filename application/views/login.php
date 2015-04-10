0ceb0c5566305ca4674b0492c5cc97e3f20f68a5
woraphon supharp <woraphon.sx@gmail.com>
2015-04-10 16:41:39 +0700

    update

--- application/views/login.php
+++ application/views/login.php
@@ -5,39 +5,41 @@
 <br>
 <br>
 <br>
-<div class="row ">
-	<div class="col-md-9">
-		<div class="row">
-			<div class="col-md-6 col-md-offset-5 bg-success">
-				<div class="col-md-12  col-md-offset-1">
-					<h1>ViA Card (Thailand)</h1>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>		
-
+	
 <div class="row ">
 	<div class="col-md-9 ">
 		<div class="row ">
-  			<div class="col-md-6 col-md-offset-5 bg-success">
+  			<div class="col-md-6 col-md-offset-5 bg-default">
+  				<div class="form-group">
+					<div class="col-md-offset-4 col-sm-8">
+  						<h2>Log in</h2>
+  					</div>
+  				</div>
+  				<br>
+  				<br>
   				<?php
   				$attributes = array('class' => 'form-horizontal', );
 
 				 echo form_open('index/login', $attributes); 	?>
-  				<div class="form-group">
-  					<div class="col-sm-12">
-  						<input type="text" class="form-control" name="username" placeholder="Enter email">
+				<div class="form-group">
+					<div class="col-md-offset-2 col-sm-8">
+  						<div class="input-group">
+  							<span class="input-group-addon glyphicon glyphicon-user" id="basic-addon1"></span>
+  							<input type="text" class="form-control glyphicon glyphicon-user col-xs-3" name="username" placeholder="E-mail" aria-describedby="basic-addon1">
+  						</div>
   					</div>
   				</div>
   				<div class="form-group">
-  					<div class="col-sm-12">
-  						<input type="password" class="form-control" name="password" placeholder="Enter password">
+  					<div class="col-md-offset-2 col-sm-8">
+  						<div class="input-group">
+  							<span class="input-group-addon glyphicon glyphicon-pencil" id="basic-addon1"></span>
+  							<input type="password" class="form-control glyphicon glyphicon-pencil col-xs-3" name="password" placeholder="Password" aria-describedby="basic-addon1">
+  						</div>
   					</div>
   				</div>
   				<div class="form-group">
-  					<div class="col-sm-12">		
-  						<button type="submit" class="btn btn-info btn-lg btn-block">login</button>
+  					<div class="col-md-offset-2 col-sm-8">		
+  						<button type="submit" class="btn btn-info btn-lg btn-block">log in</button>
   					</div>
   				</div>
   			</div>
