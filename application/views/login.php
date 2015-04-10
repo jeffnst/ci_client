7a055eb6e0026ec563fafd5331aa93436658388b
woraphon supharp <woraphon.sx@gmail.com>
2015-04-10 18:04:05 +0700

    update

--- application/views/login.php
+++ application/views/login.php
@@ -2,21 +2,21 @@
 <?php $this->view('include/header');?>
 
 <div class="container">
-<br>
-<br>
-<br>
 	
 <div class="row ">
 	<div class="col-md-9 ">
 		<div class="row ">
-  			<div class="col-md-6 col-md-offset-5 bg-default">
+			<div class="col-md-6 col-md-offset-5  box-head">
+				<div class="col-md-12 col-sm-12 ">	
+						<label style="text-align: center; margin-left:5%"><h1>ViA Card (Thailand)</h1></label>
+				</div>
+			</div>
+  			<div class="col-md-6 col-md-offset-5 bg-default box">
   				<div class="form-group">
 					<div class="col-md-offset-4 col-sm-8">
   						<h2>Log in</h2>
   					</div>
   				</div>
-  				<br>
-  				<br>
   				<?php
   				$attributes = array('class' => 'form-horizontal', );
 
@@ -42,11 +42,21 @@
   						<button type="submit" class="btn btn-info btn-lg btn-block">log in</button>
   					</div>
   				</div>
+  				<div class="form-group">
+  					<div class="col-md-offset-5 col-sm-8">		
+  						<label>or</label>
+  					</div>
+  				</div>
+  				<div class="form-group">
+  					<div class="col-md-offset-2 col-sm-8">		
+  						<button type="submit" class="btn btn-primary btn-lg btn-block">Log in with Google</button>
+  					</div>
+  				</div>
   			</div>
-  			<div class="col-md-9 col-md-offset-3">
+  			<div class="col-md-10 col-md-offset-3">
   				<hr/>
   			</div>
-  			<div class="col-md-9 col-md-offset-5">
+  			<div class="col-md-12 col-md-offset-5">
   				
   				<label>EX.</label>
   				<label>username -> user@example.com</label>
